cc002ee6ca61b8b820affdc94f749df14b3b00df
MattCollins84 <matt@strikesandgutters.com>
2018-05-14 10:26:17 +0100

    camera stream via YOLO

--- build/webcam.js
+++ build/webcam.js
@@ -1,16 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Dependencies
- */
 const cv_analytics_lib_1 = require("cv-analytics-lib");
 const { grabFrames } = cv_analytics_lib_1.Utils;
-const items = new cv_analytics_lib_1.BGItemCollection(cv_analytics_lib_1.Item);
-grabFrames('rtsp://admin:Cloudview2018!@10.0.1.130/Streaming/Channels/101/', 5, (frame) => {
-    const filter = (rect) => {
-        return rect.area >= 4000;
-    };
-    // detect the items on screen
-    items.detect(frame, { filter, lowerThreshold: 200, upperThreshold: 255 });
+const opts = {
+    source: 'rtsp://admin:Cloudview2018@10.0.1.130/Streaming/Channels/101/',
+    delay: 15
+};
+const items = new cv_analytics_lib_1.VideoClassifyItemCollection(cv_analytics_lib_1.Item, opts);
+const stream = items.getStream();
+var res;
+var frameBuffer = [];
+stream.on('data', data => {
+    res = data;
+});
+grabFrames(opts.source, 40, (frame) => {
+    frameBuffer.push(frame);
+    if (frameBuffer.length <= 15)
+        return;
+    let theFrame = frameBuffer.shift();
+    if (res && res.detections && res.detections.misc) {
+        res.detections.misc.forEach(detection => {
+            detection = new cv_analytics_lib_1.Detection(detection);
+            theFrame = detection.drawOutline(theFrame);
+        });
+    }
+    // put into window
+    cv_analytics_lib_1.cv.imshow('Tracking', theFrame);
 });
 //# sourceMappingURL=webcam.js.map
--- src/webcam.ts
+++ src/webcam.ts
@@ -1,19 +1,34 @@
-/**
- * Dependencies
- */
-import { Utils, BGItemCollection, Item } from 'cv-analytics-lib';
+import { VideoClassifyItemCollection, Item, Utils, cv, Detection } from 'cv-analytics-lib';
 const { grabFrames } = Utils;
 
-const items = new BGItemCollection<Item>(Item);
-grabFrames('rtsp://admin:Cloudview2018!@10.0.1.130/Streaming/Channels/101/', 5, (frame) => {
+const opts = {
+  source: 'rtsp://admin:Cloudview2018@10.0.1.130/Streaming/Channels/101/',
+  delay: 15
+}
+const items = new VideoClassifyItemCollection<Item>(Item, opts);
+const stream = items.getStream();
+var res;
+var frameBuffer = [];
+stream.on('data', data => {
+  res = data;
+})
 
-  const filter = (rect) => {
-    return rect.area >= 4000;
+grabFrames(opts.source, 40, (frame) => {
+
+  frameBuffer.push(frame);
+
+  if (frameBuffer.length <= 15) return;
+
+  let theFrame = frameBuffer.shift();
+
+  if (res && res.detections && res.detections.misc) {
+    res.detections.misc.forEach(detection => {
+      detection = new Detection(detection);
+      theFrame = detection.drawOutline(theFrame)
+    })
   }
 
-  // detect the items on screen
-  items.detect(frame, { filter, lowerThreshold: 200, upperThreshold: 255 });
+  // put into window
+  cv.imshow('Tracking', theFrame);
   
 });
-
-
